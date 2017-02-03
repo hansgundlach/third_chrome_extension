42a723786fe82e73c4d4c6c83e3c3b287070aed7
Hans Gundlach <hans.gundlach@gmail.com>
2017-02-02 22:11:49 -0800

    integrating wiki API

--- script.js
+++ script.js
@@ -21,7 +21,39 @@ $.each(acro, function(key, value) {
 
        	  $("p").highlight(key, {caseSensitive: false, className: 'yellowhigh', wordsOnly:true });
 
+
+      /*    $("p").LiteTooltip({
+        textalign: 'left',
+        trigger: 'hoverable',
+        location: "right",
+        padding: 5,
+        title:
+        '<div class="video-wrapper">' +
+        '<iframe src="EMBED VIDEO URL" scrolling="no" frameborder="0" allowfullscreen mozallowfullscreen webkitallowfullscreen>' +
+        '</iframe>' +
+        '</div>'
+    });*/
+
+
+
+
     });
+
+//adding the one below for kicks
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     $('.yellowhigh').each(function() {
@@ -32,7 +64,24 @@ $.each(acro, function(key, value) {
 
 			//Tipped.create(this, 'Some tooltip text');
 
-			Tipped.create(this, {
+
+      var jSONdata;
+      var data;
+      $.getJSON('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=Stack%20Overflow', function(result) {
+          //data is the JSON string
+      /*    $.each(result, function(i, field){
+                       console.log(i);
+                       jSONdata = field;
+          });*/
+      // console.log(result.query.pages.extract);
+      //console.log(result);
+    data =  result.query.pages[21721040].extract
+    //  console.log(result.query.pages.extract);
+      });
+console.log(data)
+
+//only available in pro version
+/*	Tipped.create(this, {
       ajax: {
         url: 'http://vimeo.com/api/oembed.json?url=http://vimeo.com/6428069&maxwidth=280&maxheight=280',
         success: function(data, textStatus, jqXHR) {
@@ -41,7 +90,15 @@ $.each(acro, function(key, value) {
             content: data.html
           };
         }
-      },
+      },*/
+
+
+
+
+
+
+
+
 
             /*close: true,
             hideOn: false,
@@ -55,12 +112,15 @@ $.each(acro, function(key, value) {
 
 
 
-    });
+
 
       $('.highlight-16235').each(function() {
 
     	var currentKey = $(this).text();
 
 
-    });
+
+
+
+
 });
