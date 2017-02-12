078930c952e857466d6aada8c8ee7efbb0609c28
Hans Gundlach <hans.gundlach@gmail.com>
2017-02-12 10:22:36 -0800

    added better mark.js library

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-
 console.log("this is script.js");
 
 jQuery(document).ready(function($) {
 
   var acro = {
-  'if' :'2',
-  'but' : '2',
-  'computer' : '3',
-  'Adam Smith' :'5'
+  'Adam' :'2',
+  'Eve' : '2',
+  'Tom' : '3',
+  'John' :'5',
+  'Hans' : '6'
 
   }
 
@@ -16,15 +16,57 @@ jQuery(document).ready(function($) {
   console.log("this is script.js");
 
 
+
+//new mark.js library
+
+
+//var regexp = "/Adam/i";
+var context = document.querySelector("p");
+  var instance = new Mark(context);
+  /*instance.mark("text", {
+      "element": "span",
+      "className": "highlight"
+  });*/
+
+  instance.markRegExp(/\w*[A-Z]\w*[A-Z]\w*/i);
+
+
+$("p").markRegExp(/\w*[A-Z]\w*[A-Z]\w*/i, {
+  "element":"span",
+  "className":"yellowhigh"
+});
+
+
+//$.each is bascilly like for loop
+//I think highlight does all the finding
 $.each(acro, function(key, value) {
-
-
-       	  $("p").highlight(key, {caseSensitive: false, className: 'yellowhigh', wordsOnly:true });
+  console.log(key);
+  console.log(value);
+       	//  $("p").highlight(key, {caseSensitive: false, className: 'yellowhigh', wordsOnly:true });
 
 
 
           //see if this accepts tooltipster
-      //    $('p').tooltipster();
+  //  $('p').tooltipster({theme: 'tooltipster-noir'}).tooltipster('This is my message');
+
+        //  $("p").prop("title","");
+
+
+
+         /*$('p').tooltipster({
+             functionInit: function(instance, helper){
+
+                 // parse the content
+                 var content = instance.content(),
+                     people = JSON.parse(content),
+                     // and use it to make a sentence
+                     newContent = 'We have ' + people.length + ' people today. Say hello to ' + people.join(', ');
+
+                 // save the edited content
+                 instance.content(newContent);
+             }
+         });*/
+
 
 
 
@@ -40,6 +82,11 @@ $.each(acro, function(key, value) {
         '</iframe>' +
         '</div>'
     });*/
+
+
+
+
+
 
 
 
@@ -62,7 +109,6 @@ $.each(acro, function(key, value) {
 
 
 
-
     $('.yellowhigh').each(function() {
 
 //this refers to the text that has just been highlighted
@@ -72,7 +118,7 @@ $.each(acro, function(key, value) {
 			//Tipped.create(this, 'Some tooltip text');
 
 
-var data;
+/*var data;
 
 
 
@@ -80,21 +126,66 @@ var data;
           async: false,
           url: "t",
           success: function(result) {
-            data =  result.query.pages[21721040].extract;
+            //data =  result.query.pages[21721040].extract;
           }
-      });
+      });*/
 
 
-          Tipped.create(this, data);
+        //  Tipped.create(this, "An algorithmn for pattern refinment");
 
-
-
-
-
-
+        var iframeWidth = '800';
+        var iframeHeight = '600';
 
 
 
+  /*      $(this).qtip({ // Grab some elements to apply the tooltip t
+          //content: $('<iframe src= "https://en.wikipedia.org/wiki/Adam_Smith?printable=yes" />')
+          content:    {
+            text: '<div><iframe id="theframe" src="https://en.wikipedia.org/wiki/Adam_Smith?printable=yes" frameBorder="100" scrolling="yes"' + 'width=' + '"' + iframeWidth + '"' + 'height' + iframeHeight
+            + '"' +
+             '><p>Your browser does not support iframes.</p></iframe></div>'
+        },                                                     //$('<iframe src="https://en.wikipedia.org/wiki/Adam_Smith?printable=yes">'),
+               position: {
+                  corner: {
+                     tooltip: 'bottomLeft',
+                     target: 'topRight'
+                  }
+               },
+
+               style: {
+
+                  name: 'dark'
+               },
+               hide: {
+         event: 'unfocus'
+    }
+
+
+        })
+*/
+
+//json content qTip2
+
+urlString = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + currentKey;
+
+     $(this).qtip({
+       content: {
+              text: 'Loading...', // Loading text...
+              ajax: {
+                  url: urlString, // URL to the JSON script
+                  type: 'GET', // POST or GET
+                  data: { id: 3 }, // Data to pass along with your request
+                  dataType: 'json', // Tell it we're retrieving JSON
+                  success: function(data, status) {
+                      var content = 'My name is ' +  JSON.stringify(data);//data.query.pages[21721040].extract;
+
+                      // Now we set the content manually (required!)
+                      this.set('content.text', content);
+                  }
+              }
+          }
+
+   })
 
 
 
@@ -106,23 +197,23 @@ var data;
 
 
 
-
-
-  /*    var jSONdata;
+/*var data;
+  var jSONdata;
 
       $.getJSON('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=Stack%20Overflow', function(result) {
           //data is the JSON string
+          console.log(result.query.pages[1]);
          $.each(result, function(i, field){
-                       console.log(i);
+                    //   console.log(i);
                        jSONdata = field;
           });
       // console.log(result.query.pages.extract);
       //console.log(result);
-    data =  result.query.pages[21721040].extract*/
-
-    //  console.log(result.query.pages.extract);
-//      });
-//console.log(data)
+    //data =  result.query.pages[21721040].extract;
+      data = result;
+   console.log(result.query.pages.extract);
+    });
+console.log(data)*/
 
 //only available in pro version
 /*	Tipped.create(this, {
